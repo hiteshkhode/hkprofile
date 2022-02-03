ee5ab4672b99e55d701174ab564f551ace32f460
Hitesh Khode <khodehitesh@gmail.com>
2022-02-03 16:31:35 +0530

    Header colored with sky-900

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+function Header(){
+    return (
+        <div className="header flex py-10 bg-sky-900">
+            <div className="grow items-center justify-center flex text-white">Home</div>
+            <div className="grow items-center justify-center flex text-white">Resume</div>
+            <div className="grow items-center justify-center flex text-white">About</div>
+            <div className="grow items-center justify-center flex text-white">Contact</div>
+        </div>
+    )
+}
+
+export default Header
