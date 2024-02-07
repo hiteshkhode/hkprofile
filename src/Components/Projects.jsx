14033aafd3a2f55664dff2ff74377c7ce597f7ad
hiteshkhode <khodehitesh@gmail.com>
2024-02-07 12:53:08 +0530

    cryptrade typo fix

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -11,7 +11,7 @@ function projects() {
                     PROJECTS
                 </div>
                 <Project title = {projectsJSON[0].title} description = {projectsJSON[0].description} links = {projectsJSON[0].link} project="blockchainWordle"/>
-                <Project title = {projectsJSON[1].title} description = {projectsJSON[1].description} links = {projectsJSON[1].link} project="crypTrade" />
+                <Project title = {projectsJSON[1].title} description = {projectsJSON[1].description} links = {projectsJSON[1].link} project="cryptrade" />
                 <Project title = {projectsJSON[2].title} description = {projectsJSON[2].description} links = {projectsJSON[2].link} project="horoscopeNFT" />
                 <Project title = {projectsJSON[3].title} description = {projectsJSON[3].description} links = {projectsJSON[3].link} project="pablo-s-palette" />
                 <Project title = {projectsJSON[4].title} description = {projectsJSON[4].description} links = {projectsJSON[4].link} project="polling-site" />
